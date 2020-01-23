55063b61b106218fcb1c8b3bc14c27c8658b319b
Joe Ellingworth <joeellingworth@gmail.com>
2020-01-23 12:00:56 +0000

    converter complete

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -17,8 +17,23 @@ describe 'RomanNumerals' do
     expect(RomanNumerals.convert(25)).to eq ("XXV")
   end 
   
-  it "Should be able to convert 53 to XXXXXIII" do 
-    expect(RomanNumerals.convert(53)).to eq ("XXXXXIII")
+  it "Should be able to convert 53 to LIII" do 
+    expect(RomanNumerals.convert(53)).to eq ("LIII")
   end 
 
+  it "Should be able to convert 39 to XXXIX" do 
+    expect(RomanNumerals.convert(39)).to eq ("XXXIX")
+  end 
+
+  it "Should be able to convert 24 to XXIV" do 
+    expect(RomanNumerals.convert(24)).to eq ("XXIV")
+  end 
+
+  it "should be able to convert 996 to CMXCVI" do
+    expect(RomanNumerals.convert(996)).to eq ("CMXCVI")
+  end 
+
+  it "should be able to convert 1992 to MCMXCII" do
+    expect(RomanNumerals.convert(1992)).to eq ("MCMXCII")
+  end 
 end
